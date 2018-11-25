8aa23f52c90f455acc2a27b9fdced4cb10379c25
MichalPC1 <banka.michal1964@gmail.com>
2018-11-25 19:23:46 +0100

    Views' errors fixed

--- SzablonAppSpringWebMVC/src/main/java/com/app/configuration/AppConfiguration.java
+++ SzablonAppSpringWebMVC/src/main/java/com/app/configuration/AppConfiguration.java
@@ -5,9 +5,12 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.spring4.SpringTemplateEngine;
@@ -60,5 +63,21 @@ public class AppConfiguration extends WebMvcConfigurerAdapter implements Applica
         resolver.setTemplateMode(TemplateMode.HTML);
         resolver.setCharacterEncoding("utf-8");
         return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(1)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+    }
+    //MULTIPART
+    @Bean
+    public StandardServletMultipartResolver multipartResolver()
+    {
+        return new StandardServletMultipartResolver();
     }
 }
--- SzablonAppSpringWebMVC/src/main/java/com/app/models/dao/CarDao.java
+++ SzablonAppSpringWebMVC/src/main/java/com/app/models/dao/CarDao.java
@@ -1,9 +1,6 @@
 package com.app.models.dao;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.Date;
