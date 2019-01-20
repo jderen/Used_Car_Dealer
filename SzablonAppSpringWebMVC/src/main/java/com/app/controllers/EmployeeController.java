f1b61770f8dd68cbd9f00859c8ca95e61b9fcff2
MichalPC1 <banka.michal1964@gmail.com>
2019-01-20 04:10:27 +0100

    controllers update

--- SzablonAppSpringWebMVC/src/main/java/com/app/controllers/EmployeeController.java
+++ SzablonAppSpringWebMVC/src/main/java/com/app/controllers/EmployeeController.java
@@ -1,15 +1,39 @@
 package com.app.controllers;
+import com.app.models.dao.EmployeeDao;
+import com.app.models.dto.CarDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.persistence.EntityManager;
 
 @Controller
 @RequestMapping("employee")
 public class EmployeeController {
 
+    private EmployeeDao employeeDao;
+
+    public EmployeeController(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
+
     @GetMapping("/")
-    public String loginPost(Model model) {
+    public String loginGet(Model model) {
+        return "employee/login";
+    }
+
+    /*todo below*/
+
+    @PostMapping("/")
+    public String loginPost(@ModelAttribute String login, @ModelAttribute String password) {
+        if(login != null && password != null && employeeDao !=null){
+            if (employeeDao.findByUsernameAndPassword(login,password).isPresent()){
+                return "employee/carsEmployeeView";
+            }
+        }
         return "employee/login";
     }
 
--- SzablonAppSpringWebMVC/src/main/java/com/app/controllers/MainController.java
+++ SzablonAppSpringWebMVC/src/main/java/com/app/controllers/MainController.java
@@ -1,14 +1,71 @@
 package com.app.controllers;
+import com.app.models.Car;
+import com.app.models.Employee;
+import com.app.models.dao.CarDao;
+import com.app.models.dao.EmployeeDao;
+import com.app.models.dao.EmployeeDaoImpl;
+import com.app.models.dto.CarDto;
+import com.app.models.dto.converters.CarConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.Null;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/")
-    public String showForm() {
-        return "carsClientView";
+    private EmployeeDao employeeDao;
+    private CarDao carDao;
+
+    public MainController(EmployeeDao employeeDao, CarDao carDao) {
+        this.employeeDao = employeeDao;
+        this.carDao = carDao;
+    }
+
+    @GetMapping("/")
+    public String showFormGet(Model model) {
+        List<Car> list = carDao.findAll();
+        List<CarDto> carsDto = new ArrayList<>();
+        CarConverter carConverter = new CarConverter();
+
+        for (Car car : list) {
+            carsDto.add(carConverter.carToCarDto(car));
+        }
+
+        model.addAttribute("cars", carsDto);
+        return "carsClientViewTL";
+    }
+
+    @GetMapping("/{id}")
+    public String showContactInfo(@PathVariable Long id, Model model){
+        if (id != null)
+        {
+            try{
+                Random random = new Random();
+                Long employeeId = (long) random.nextInt(employeeDao.count().intValue()) + 1;
+
+                Employee employee = employeeDao.findById(employeeId).orElseThrow(NullPointerException::new);
+                model.addAttribute("e", employee);
+                model.addAttribute("id", id);
+            }catch (NullPointerException e){
+                System.out.println(e.toString());
+                System.out.println("NULL POINTER EXCEPTION - SHOW CONTACT INFO");
+            }
+
+            return "carContactInfo";
+        }
+        return "redirect:/";
     }
 }
